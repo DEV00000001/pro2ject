6dc19b3916ee3286781bcd92a3d52e84144671da
DEV00000001 <vuongdachaivang@gmail.com>
2025-07-18 18:12:34 +0700

    formik component : Formik , Form , Field , ErrorMessage

--- src/components/PopupForm/index.jsx
+++ src/components/PopupForm/index.jsx
@@ -1,24 +1,12 @@
 // components/Popup.jsx
 import "./PopupForm.scss";
-import { useFormik } from "formik";
-import * as Yup from 'yup';
-const validate = (values) => {
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required!";
-  }
-  if (!values.code) {
-    errors.code = "Required";
-  }
-  if (!values.description) {
-    errors.description = "Required";
-  }
-  return errors;
-};
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 const validationSchema = Yup.object({
-  name: Yup.string().required('Required!'),
-  code: Yup.string().required('Required!'),
-  description: Yup.string().required('Required!'),
+  name: Yup.string().required("Required!"),
+  code: Yup.string().required("Required!"),
+  description: Yup.string().required("Required!"),
 });
 function PopupForm({ open, onClose, country, onSave, type }) {
   if (!open) return null;
@@ -37,76 +25,76 @@ function PopupForm({ open, onClose, country, onSave, type }) {
     }
     onSave(newValues);
   };
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    // validate,
-    validationSchema
-  });
-  console.log(formik.touched)
   return (
     <div className="popup__overlay">
       <div className="popup__content">
         <h2 className="popup__title">
           {type == "edit" ? "Edit Country" : "Add Country"}
         </h2>
-        <form className="popup__form" onSubmit={formik.handleSubmit}>
-          <div className="popup__form-group">
-            <label>Name:</label>
-            <input
-              className="popup__form-input"
-              name="name"
-              type="text"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.name && formik.errors.name ? (
-              <div className="popup__errors">{formik.errors.name}</div>
-            ) : null}
-          </div>
-          <div className="popup__form-group">
-            <label>Code:</label>
-            <input
-              className="popup__form-input"
-              type="text"
-              name="code"
-              value={formik.values.code}
-              onChange={formik.handleChange}
-              // focus vao sau do unfocus(an ra ben ngoai) => set formik.touched.[name] = true
-              onBlur={formik.handleBlur}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <Form className="popup__form" >
+            <div className="popup__form-group">
+              <label for="name">Name:</label>
+              <Field
+                className="popup__form-input"
+                id="name"
+                name="name"
+                type="text"
+                // ko su dung getFieldProps neu su dung component UI cua frameworkUI (Antd, MUI)
+                // formik.getFieldProps => cac props cua field (onChange={formik.handleChange} .....)
+                // formik.handleChange chi hoat dong voi HTML thuan
+                // doi vs component UI cua framework su dung setFieldValues
+                // {...formik.getFieldProps("name")}
+              />
+              <ErrorMessage name="name" />
+              {/* {formik.touched.name && formik.errors.name ? (
+                <div className="popup__errors">{formik.errors.name}</div>
+              ) : null} */}
+            </div>
+            <div className="popup__form-group">
+              <label for="code">Code:</label>
+              <Field
+                className="popup__form-input"
+                type="text"
+                id="code"
+                name="code"
+                // value={formik.values.code}
+                // onChange={formik.handleChange}
+                // // focus vao sau do unfocus(an ra ben ngoai) => set formik.touched.[name] = true
+                // onBlur={formik.handleBlur}
 
-            />
-            {formik.touched.code && formik.errors.code ? (
-              <div className="popup__errors">{formik.errors.code}</div>
-            ) : null}
-          </div>
+                // {...formik.getFieldProps("code")}
+              />
+              <ErrorMessage name="code"/>
+            </div>
 
-          <div className="popup__form-group">
-            <label>Description:</label>
-            <textarea
-              className="popup__form-input"
-              name="description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.description && formik.errors.description ? (
-              <div className="popup__errors">{formik.errors.description}</div>
-            ) : null}
-          </div>
-          <div className="popup__form-actions">
-            <button
-              className="popup__form-button popup__form-button--red"
-              type="submit"
-            >
-              Save
-            </button>
-            <button className="popup__form-button" onClick={onClose}>
-              Cancel
-            </button>
-          </div>
-        </form>
+            <div className="popup__form-group">
+              <label for="description">Description:</label>
+              <Field
+                component="textarea"
+                className="popup__form-input"
+                name="description"
+                id="description"
+              />
+              <ErrorMessage name="description"/>
+            </div>
+            <div className="popup__form-actions">
+              <button
+                className="popup__form-button popup__form-button--red"
+                type="submit"
+              >
+                Save
+              </button>
+              <button className="popup__form-button" onClick={onClose}>
+                Cancel
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
