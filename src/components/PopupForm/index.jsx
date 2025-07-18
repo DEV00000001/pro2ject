391b53f04307e9751f4f1eb47cb3b3aeeb4404cb
DEV00000001 <vuongdachaivang@gmail.com>
2025-07-18 17:36:01 +0700

    formik basic

--- src/components/PopupForm/index.jsx
+++ src/components/PopupForm/index.jsx
@@ -1,39 +1,69 @@
 // components/Popup.jsx
 import "./PopupForm.scss";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required!";
+  }
+  if (!values.code) {
+    errors.code = "Required";
+  }
+  if (!values.description) {
+    errors.description = "Required";
+  }
+  return errors;
+};
+const validationSchema = Yup.object({
+  name: Yup.string().required('Required!'),
+  code: Yup.string().required('Required!'),
+  description: Yup.string().required('Required!'),
+});
 function PopupForm({ open, onClose, country, onSave, type }) {
   if (!open) return null;
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let updatedCountry = {
-      name: e.target[0].value,
-      code: e.target[1].value,
-      description: e.target[2].value,
-    };
+  const initialValues = {
+    name: country?.name || "hehe",
+    code: country?.code || "",
+    description: country?.description || "",
+  };
+  const onSubmit = (values) => {
+    let newValues = values;
     if (type === "edit") {
-      updatedCountry = {
+      newValues = {
         id: country.id,
-        
-        ...updatedCountry,
+        ...values,
       };
     }
-    onSave(updatedCountry);
+    onSave(newValues);
   };
-
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    // validate,
+    validationSchema
+  });
+  console.log(formik.touched)
   return (
     <div className="popup__overlay">
       <div className="popup__content">
         <h2 className="popup__title">
           {type == "edit" ? "Edit Country" : "Add Country"}
         </h2>
-        <form className="popup__form" onSubmit={handleSubmit}>
+        <form className="popup__form" onSubmit={formik.handleSubmit}>
           <div className="popup__form-group">
             <label>Name:</label>
             <input
               className="popup__form-input"
               name="name"
               type="text"
-              defaultValue={country?.name}
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="popup__errors">{formik.errors.name}</div>
+            ) : null}
           </div>
           <div className="popup__form-group">
             <label>Code:</label>
@@ -41,8 +71,15 @@ function PopupForm({ open, onClose, country, onSave, type }) {
               className="popup__form-input"
               type="text"
               name="code"
-              defaultValue={country?.code}
+              value={formik.values.code}
+              onChange={formik.handleChange}
+              // focus vao sau do unfocus(an ra ben ngoai) => set formik.touched.[name] = true
+              onBlur={formik.handleBlur}
+
             />
+            {formik.touched.code && formik.errors.code ? (
+              <div className="popup__errors">{formik.errors.code}</div>
+            ) : null}
           </div>
 
           <div className="popup__form-group">
@@ -50,8 +87,13 @@ function PopupForm({ open, onClose, country, onSave, type }) {
             <textarea
               className="popup__form-input"
               name="description"
-              defaultValue={country?.description}
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.description && formik.errors.description ? (
+              <div className="popup__errors">{formik.errors.description}</div>
+            ) : null}
           </div>
           <div className="popup__form-actions">
             <button
